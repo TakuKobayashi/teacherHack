228f67d57ede3d0445124904958abd090c1c33ee
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2015-10-11 12:37:34 +0900

    loginとsign_upをpassport使って実装できるようにした

--- api/models/User.js
+++ api/models/User.js
@@ -4,11 +4,40 @@
 * @description :: TODO: You might write a short summary of how this model works and what it represents here.
 * @docs        :: http://sailsjs.org/#!documentation/models
 */
+var bcrypt = require('bcrypt');
 
 module.exports = {
 
   attributes: {
-
+    name: { type: 'string'},
+    sessionToken: { type: 'string'},
+    lastLoginedAt: { type: 'datetime'},
+    mailAddress: { type: 'string'},
+    password: { 
+      type: 'string',
+      minLength: 6,
+      required: true
+    },
+    sessionToken: { type: 'string'},
+    updatedAt: { type: 'datetime'},
+    createdAt: { type: 'datetime'},
+    toJSON: function() {
+      var obj = this.toObject();
+      return obj;
+    }
+  },
+  beforeCreate: function(user, cb) {
+    bcrypt.genSalt(10, function(err, salt) {
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if (err) {
+          console.log(err);
+          cb(err);
+        } else {
+          user.password = hash;
+          user.lastLoginedAt = new Date();
+          cb();
+        }
+      });
+    });
   }
 };
-
---
+++ config/passport.js
@@ -0,0 +1,43 @@
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var bcrypt = require('bcrypt');
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findOne({ id: id } , function (err, user) {
+        done(err, user);
+    });
+});
+
+passport.use(new LocalStrategy({
+    usernameField: 'mailAddress',
+    passwordField: 'password'
+  },
+  function(mailAddress, password, done) {
+
+    User.findOne({ mailAddress: mailAddress }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect mailAddress.' });
+      }
+
+      bcrypt.compare(password, user.password, function (err, res) {
+          if (!res)
+            return done(null, false, {
+              message: 'Invalid Password'
+            });
+          var returnUser = {
+            mailAddress: user.mailAddress,
+            createdAt: user.createdAt,
+            id: user.id
+          };
+          return done(null, returnUser, {
+            message: 'Logged In Successfully'
+          });
+        });
+    });
+  }
+));
